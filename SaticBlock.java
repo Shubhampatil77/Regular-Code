c2366edc6dde739f875ccd4c5b2b5b0a8b5e7e55
Shubham <126310585+Shubhampatil77@users.noreply.github.com>
2023-09-25 22:00:33 +0530

    Add files via upload

---
+++ SaticBlock.java
@@ -0,0 +1,17 @@
+package Day5;
+
+public class SaticBlock {
+
+	static {
+		System.out.println("I am static block");
+
+	}
+
+	public static void main(String args[]) {
+		System.out.println("I am Main Method");
+	}
+	static {
+		System.out.println("I am static block 2");
+
+	}
+}
