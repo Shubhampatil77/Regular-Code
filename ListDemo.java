0a4f7a6e22054a7d3bb1e67c45fd2ef885e9b5fd
Shubham <126310585+Shubhampatil77@users.noreply.github.com>
2023-09-26 16:10:41 +0530

    Add files via upload

---
+++ ListDemo.java
@@ -0,0 +1,22 @@
+package Day6Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+	public static void main(String args[]) {
+		List l=new ArrayList();
+		l.add(11);
+		l.add(21);
+		l.add(21);
+		l.add(21);
+		l.add(21);
+		l.add(21);
+		l.add(21);
+		l.add(21);
+		l.add(22);
+		
+		System.out.print(l);
+	}
+
+}
