52c9c058a100506696d358d5923637a3c4b81b6c
Shubham <126310585+Shubhampatil77@users.noreply.github.com>
2023-09-28 09:33:59 +0530

    Add files via upload

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -1,4 +1,4 @@
-package Day6Collection;
+package Day7Collection;
 
 import java.util.LinkedList;
 
@@ -7,10 +7,10 @@ public class LinkedListDemo {
 		LinkedList l = new LinkedList();
 		l.add(100);
 		l.add(200);
-		l.add(300);
 		l.add("Shubham");
 		l.add("Deepak");
 		l.add("Deepak");
+		l.add(300);
 		l.add(500);
 
 		System.out.println(l);
@@ -21,8 +21,13 @@ public class LinkedListDemo {
 		ll.add(40);
 
 		System.out.println(ll);
-		System.out.println(ll.getFirst());
-		System.out.println(ll.getLast());
+		System.out.println("Before Fierst Elemnt of ll=  "+ll.getFirst());
+		System.out.println("Before last Elemnt of ll=  "+ll.getLast());
+		ll.removeFirst();
+		ll.removeLast();
+		
+		System.out.println("After Fierst Elemnt of ll=  "+ll.getFirst());
+		System.out.println("After last Elemnt of ll=  "+ll.getLast());
 
 		System.out.println(l.contains(300));
 		System.out.print(l.containsAll(ll));
---
+++ VectorDemo.java
@@ -0,0 +1,29 @@
+package Day7Collection;
+
+import java.util.Vector;
+
+public class VectorDemo {
+	public  static void main(String args[]) {
+		Vector v=  new Vector();
+		v.add(100);
+		v.add(200);
+		v.add(300);
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		v.add("Shubham");
+		
+		System.out.println(v);
+		System.out.println(v.capacity());
+
+	}
+
+}
