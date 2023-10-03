5aedc4a4b8ed2e819792da178c525ee6251269c4
Shubham <126310585+Shubhampatil77@users.noreply.github.com>
2023-10-03 20:26:24 +0530

    Add files via upload

---
+++ SQL.java
@@ -0,0 +1,26 @@
+package jdbc;
+import java.sql.*;
+public class SQL {
+	public static void main(String[] args) 
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/shubham","root","1234");
+			
+			Statement stat=con.createStatement();
+			ResultSet rs=stat.executeQuery("select * from student");
+			
+			while(rs.next())
+				{
+				System.out.println(rs.getInt(1)+"    "+rs.getString(2)+"  "+rs.getInt(3));
+				}
+			rs.close();
+			con.close();
+		}
+		catch(Exception e)
+		{
+			System.out.print(e);
+		}
+	}
+}
