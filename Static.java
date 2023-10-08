8bbe53d069ce7d18d67d83b8c97e9b0e57807402
Shubham <126310585+Shubhampatil77@users.noreply.github.com>
2023-10-08 22:39:06 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,23 @@
+package LabManualDemo;
+
+public class Static {
+	
+	
+	public static void main(String args[]) {
+		A r= new A();
+		
+			
+	
+	{
+		System.out.println("Static 1st Called");
+	}
+	 A
+{
+	System.out.println("Static 2nd Called");
+}
+	static {
+		System.out.println("Static Block Called");
+	}
+
+
+}
