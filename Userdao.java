2a76685a6a983346bf8dcba184cf67429643143f
Shubham <126310585+Shubhampatil77@users.noreply.github.com>
2023-11-05 21:08:02 +0530

    Add files via upload

---
+++ Userdao.java
@@ -0,0 +1,9 @@
+package dao;
+
+import java.sql.DriverManager;
+
+public class Userdao {
+	private String jdbcURL = "jdbc:mysql://localhost:3306/shubham?userSSL=false";
+	private String jdbcUsername="root";
+	private String jdbcPassword="1234";
+}
